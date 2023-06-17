2d3fb176359bf81227cae535ad51294576c80ee6
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-17 13:48:13 +0100

    cargo build up route created

---
+++ model/cargoBuildupModel.js
@@ -0,0 +1,59 @@
+import mongoose, {Schema} from "mongoose";
+
+const CargoBuildUpSchema = new mongoose.Schema(
+    {
+        palletNo:{
+            type:String,
+            required:[true, ""]
+        },
+        airwaybill:{
+            type:String,
+            required:[true, "Airwaybill number is required"]
+        },
+        peice:{
+            type:String,
+            required:[true, ""]
+        },
+        weight:{
+            type:String,
+            required:[true, ""]
+        },
+        remark:{
+            type:String,
+            required:[true, ""]
+        },
+        origin:{
+            type:String,
+            required:[true, ""]
+        },
+        destination:{
+            type:String,
+            required:[true, ""]
+        },
+        date:{
+        },
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:[true,""]
+        },
+        palletInventory:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Invent",
+            required:[true,""]
+        },
+        awbInventory:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Awb",
+            required:[true,""]
+        },
+    },
+    {
+        timestamps:true,
+        toJSON:{virtuals:true}
+    }
+);
+
+const CargoBuildUp = mongoose.Model("CargoBuildUp", CargoBuildUpSchema)
+
+export default CargoBuildUp;
